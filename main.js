e23624e06b6ded46ce32f588a087fb85a2379702
muchala <139487633+muchala@users.noreply.github.com>
2024-08-13 18:44:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let numbers = [1, 3, 5, 7, 9];
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+
+console.log(max);
